b2a1083defbce22d74f4ce0b314f1e0abcc9b8b6
starlit-nox <kdkelly.1998@gmail.com>
2023-05-30 14:49:09 -0500

    Added State

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    }
-  }
+const BestBooks = () => {
+  const [books, setBooks] = useState([]);
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  useEffect(() => {
+    // Make a GET request to your API to fetch all the books
+    fetch('/api/books') // Replace '/api/books' with your actual API endpoint
+      .then(response => response.json())
+      .then(data => {
+        setBooks(data); // Update the books state with the fetched data
+      })
+      .catch(error => {
+        console.log('Error fetching books:', error);
+      });
+  }, []);
 
-  render() {
+  return (
+    <>
+      <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-    /* TODO: render all the books in a Carousel */
-
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
-    )
-  }
+      {books.length ? (
+        /* Render the books in a Carousel component or any other desired UI */
+        <div>
+          {books.map(book => (
+            <div key={book._id}>
+              <h3>{book.title}</h3>
+              <p>{book.description}</p>
+              <p>Status: {book.status}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h3>No Books Found :(</h3>
+      )}
+    </>
+  );
 }
 
 export default BestBooks;
